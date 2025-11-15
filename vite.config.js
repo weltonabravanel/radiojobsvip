96616de955cba99a4797c90afedc30b00ad2d027
welton kleber esteves primo <75637988+weltonabravanel@users.noreply.github.com>
2025-11-15 17:00:20 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
+// vite.config.js
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-    base: "/radiojobsvip",
-  },
+  base: "/nome-do-repositorio/", // <-- Adicione esta linha
 });
